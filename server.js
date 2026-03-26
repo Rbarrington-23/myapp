9530b889c7b155e700ec722f1bba8c22a666cfd3
Rbarrington-23 <renishabarrington3@gmail.com>
2026-03-26 11:08:48 -0400

    update project

--- server.js
+++ server.js
@@ -1,39 +1,51 @@
 const express = require("express")
 const sqlite3 = require("sqlite3").verbose()
 const path = require("path")
+const cors = require("cors")
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
+app.use(cors())
+app.use(express.json())
 app.use(express.static("public"))
 
 const db = new sqlite3.Database("./database.db")
 
 db.serialize(() => {
 
-db.run(`CREATE TABLE IF NOT EXISTS users (
+db.run(`
+CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 username TEXT,
 password TEXT
-)`)
+)
+`)
 
-db.run(`CREATE TABLE IF NOT EXISTS notes (
+db.run(`
+CREATE TABLE IF NOT EXISTS notes(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 username TEXT,
 note TEXT
-)`)
+)
+`)
 
 })
 
-app.post("/register", (req,res)=>{
+app.post("/register",(req,res)=>{
 
 const {username,password} = req.body
 
-db.run("INSERT INTO users (username,password) VALUES (?,?)",
-[username,password])
-
-res.send("Registered")
+db.run(
+"INSERT INTO users(username,password) VALUES (?,?)",
+[username,password],
+(err)=>{
+if(err){
+res.json({success:false})
+}else{
+res.json({success:true})
+}
+}
+)
 
 })
 
@@ -45,14 +57,13 @@ db.get(
 "SELECT * FROM users WHERE username=? AND password=?",
 [username,password],
 (err,row)=>{
-
 if(row){
 res.json({success:true})
 }else{
 res.json({success:false})
 }
-
-})
+}
+)
 
 })
 
@@ -61,11 +72,12 @@ app.post("/addNote",(req,res)=>{
 const {username,note} = req.body
 
 db.run(
-"INSERT INTO notes (username,note) VALUES (?,?)",
-[username,note]
+"INSERT INTO notes(username,note) VALUES (?,?)",
+[username,note],
+()=>{
+res.json({success:true})
+}
 )
-
-res.send("Note saved")
 
 })
 
@@ -75,10 +87,9 @@ db.all(
 "SELECT * FROM notes WHERE username=?",
 [req.params.username],
 (err,rows)=>{
-
 res.json(rows)
-
-})
+}
+)
 
 })
 
